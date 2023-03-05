fddfb8abe92de6e653c593158ddc2f821b743769
Sekou S. Seh <sehsekou98@gmail.comh>
2023-03-05 23:18:13 +0100

    post

--- prisma/client.js
+++ prisma/client.js
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.client
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
 export default client
-// use `prisma` in your application to read and write data in your DB
